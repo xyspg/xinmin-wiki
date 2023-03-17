150c569781071e5c0ec4d95cc4e0f9c49b5864e1
Kang Jiaming <kangjm2005@hotmail.com>
2023-03-17 20:31:28 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Xinmin-Wiki
+This is the offcial wiki of Affiliated High School of Peking University (BDFZ) Xinmin House. 
+
---
+++ docusaurus.config.js
@@ -0,0 +1,125 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: '新民书院史',
+  tagline: 'Xinmin Wiki',
+  favicon: 'img/favicon.png',
+
+  // Set the production url of your site here
+  url: 'https://xinmin-wiki.vercel.app',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh',
+    locales: ['zh'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      // image: '',
+      navbar: {
+        title: '新民书院史',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/favicon.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: '🦄️历史️',
+          },
+          {to: '/blog', label: '🦄️动态', position: 'left'},
+        ],
+      },
+      footer: {
+        style: 'light',
+        links: [
+          {
+            title: '🦄️ 历史',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+              {
+                label: '放点什么',
+                to: '/docs/intro',
+              },
+              {
+                label: '一些内容',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: '链接',
+            items: [
+              {
+                label: 'BiliBili',
+                href: 'https://space.bilibili.com/119700894/',
+              },
+            ],
+          },
+          {
+            title: '更多',
+            items: [
+              {
+                label: '🦄️动态',
+                to: '/blog',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Xinmin House`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
