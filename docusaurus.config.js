543c16621cdb221a94d703e7cbed9b7dcebda86a
Kang Jiaming <kangjm2005@hotmail.com>
2023-03-17 21:03:07 +0800

    noindex

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,6 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
+  noIndex: true,
   title: 'XM Wiki',
   tagline: '新民书院史',
   favicon: 'img/favicon.png',
